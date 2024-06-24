4193292c2c028378fa6a5229372d593fe1be2c45
Kristian <krisiyotsov@gmail.com>
2024-06-24 23:26:42 +0300

    .

--- frontend/urbanutopia/src/components/Nav.vue
+++ frontend/urbanutopia/src/components/Nav.vue
@@ -1,17 +1,43 @@
 <template>
     <nav>
-        <img src="" alt="">
+        <img :src="logo" alt="" class="img">
         <div class="btns">
             <a href="">login</a>
         </div>
     </nav>
 </template>
 
-<script></script>
+
+<script>
+import logo from '@/assets/UrbanUtopia-logo.png'; // Adjust path according to your project structure
+
+export default {
+  data() {
+    return {
+      logo: logo
+    } 
+  }
+}
+</script>
 
 <style scoped>
     nav {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         margin: 0;
         background-color: black;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-right: 1%;
+    }
+
+    .img {
+        height: 8%;
+        width: 8%;
+    }
+
+    .btns a {
+        text-decoration: none;
+        color: white;
     }
 </style>
